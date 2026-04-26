4adb084c40b74f077de599c5aeb81ed66c565282
نام خودت <mamasaremi2@gmail.com>
2026-04-26 19:54:06 +0330

    Fix main jsx entry

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { AppProvider } from "./app/providers/AppProvider";
-import { App } from "./app/App";
-import "./styles/globals.css";
+import App from "./App";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <AppProvider>
-      <App />
-    </AppProvider>
+    <App />
   </React.StrictMode>
 );
