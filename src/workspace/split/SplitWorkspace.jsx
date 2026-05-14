1c76530ff6c177de6ba33313a24c26671565c537
نام خودت <mamasaremi2@gmail.com>
2026-05-14 22:43:11 +0330

    Add SHIL V15 terminal workspace querybuilder and report infrastructure

---
+++ src/workspace/split/SplitWorkspace.jsx
@@ -0,0 +1,36 @@
+﻿import React from "react";
+
+import {
+  Panel,
+  PanelGroup,
+  PanelResizeHandle,
+} from "react-resizable-panels";
+
+export default function SplitWorkspace() {
+
+  return (
+
+    <PanelGroup direction="horizontal">
+
+      <Panel defaultSize={50}>
+
+        <div className="panel-v15">
+          LEFT
+        </div>
+
+      </Panel>
+
+      <PanelResizeHandle />
+
+      <Panel defaultSize={50}>
+
+        <div className="panel-v15">
+          RIGHT
+        </div>
+
+      </Panel>
+
+    </PanelGroup>
+
+  );
+}
