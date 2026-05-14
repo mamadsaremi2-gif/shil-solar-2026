c2aee013eba6bf089c2ed1a2ab4e9b9f5bc986fc
نام خودت <mamasaremi2@gmail.com>
2026-05-14 20:29:56 +0330

    Add SHIL V15 advanced ui infrastructure pack

---
+++ src/constants/projectConstants.js
@@ -0,0 +1,19 @@
+﻿export const PROJECT_TYPES = [
+  "آنگرید",
+  "آفگرید",
+  "هیبرید",
+  "برق اضطراری",
+];
+
+export const BATTERY_TYPES = [
+  "Lithium",
+  "GEL",
+  "AGM",
+  "Tubular",
+];
+
+export const INVERTER_TYPES = [
+  "Hybrid",
+  "Off-grid",
+  "On-grid",
+];
---
+++ src/lib/api/client.js
@@ -0,0 +1,7 @@
+﻿import axios from "axios";
+
+const api = axios.create({
+  timeout: 15000,
+});
+
+export default api;
