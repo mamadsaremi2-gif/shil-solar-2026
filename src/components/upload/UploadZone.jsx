b827259f7fd57c108d43af26335900da1300efa9
نام خودت <mamasaremi2@gmail.com>
2026-05-14 21:20:38 +0330

    Add SHIL V15 analytics and reporting infrastructure

---
+++ src/components/upload/UploadZone.jsx
@@ -0,0 +1,19 @@
+﻿import React from "react";
+import { UploadCloud } from "lucide-react";
+
+export default function UploadZone() {
+
+  return (
+    <div className="upload-zone-v15">
+
+      <UploadCloud size={42} />
+
+      <h3>آپلود فایل پروژه</h3>
+
+      <p>
+        PDF، DWG، XLSX یا تصاویر سایت پروژه
+      </p>
+
+    </div>
+  );
+}
