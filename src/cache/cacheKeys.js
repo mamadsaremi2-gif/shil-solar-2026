2a919fa5fe5944cd42ba0e1564f80cea018c1fc3
نام خودت <mamasaremi2@gmail.com>
2026-05-15 02:03:27 +0330

    Add SHIL V15 offline storage sync and encryption infrastructure

---
+++ src/cache/cacheKeys.js
@@ -0,0 +1,12 @@
+﻿export const cacheKeys = {
+
+  PROJECTS:
+    "shil_projects",
+
+  SETTINGS:
+    "shil_settings",
+
+  WEATHER:
+    "shil_weather",
+
+};
---
+++ src/offline/registerSW.js
@@ -0,0 +1,14 @@
+﻿import { Workbox }
+from "workbox-window";
+
+export function registerSW() {
+
+  if ("serviceWorker" in navigator) {
+
+    const wb =
+      new Workbox("/sw.js");
+
+    wb.register();
+
+  }
+}
