ff65fb9925b920b43fabac6cb4a942a51f543e50
نام خودت <mamasaremi2@gmail.com>
2026-05-15 02:55:08 +0330

    Add SHIL V15 realtime voice video and state machine infrastructure

---
+++ src/audio/notificationSound.js
@@ -0,0 +1,11 @@
+﻿import { Howl }
+from "howler";
+
+export const notificationSound =
+  new Howl({
+
+    src: ["/sounds/notify.mp3"],
+
+    volume: 0.5,
+
+  });
---
+++ src/audio/waveform.js
@@ -0,0 +1,19 @@
+﻿import WaveSurfer
+from "wavesurfer.js";
+
+export function createWave(
+  container
+) {
+
+  return WaveSurfer.create({
+
+    container,
+
+    waveColor:
+      "#38bdf8",
+
+    progressColor:
+      "#7c3aed",
+
+  });
+}
