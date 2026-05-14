4f977ad7b7224889254cab1e3428a802ce870236
نام خودت <mamasaremi2@gmail.com>
2026-05-15 02:36:37 +0330

    Add SHIL V15 planner table calendar and drag workspace infrastructure

---
+++ src/timer/SHILTimer.jsx
@@ -0,0 +1,32 @@
+﻿import React from "react";
+
+import {
+  useTimer
+} from "react-timer-hook";
+
+export default function SHILTimer() {
+
+  const expiryTimestamp =
+    new Date();
+
+  expiryTimestamp.setSeconds(
+    expiryTimestamp.getSeconds() + 600
+  );
+
+  const {
+    seconds,
+    minutes,
+  } = useTimer({
+    expiryTimestamp,
+  });
+
+  return (
+
+    <div className="timer-v15">
+
+      {minutes}:{seconds}
+
+    </div>
+
+  );
+}
