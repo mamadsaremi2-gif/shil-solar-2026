55aa2464c167283d43c847c49da50166fa83d4ca
نام خودت <mamasaremi2@gmail.com>
2026-05-15 16:45:18 +0330

    Add SHIL V15 fleet monitoring native mobile and commissioning layer

---
+++ src/mobile/native/nativeBridge.js
@@ -0,0 +1,18 @@
+﻿import { Device } from "@capacitor/device";
+import { Network } from "@capacitor/network";
+
+export async function getNativeDeviceInfo() {
+
+  const info =
+    await Device.getInfo();
+
+  return info;
+}
+
+export async function getNetworkStatus() {
+
+  const status =
+    await Network.getStatus();
+
+  return status;
+}
