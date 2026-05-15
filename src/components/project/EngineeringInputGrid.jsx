65477e0624e1b46a11b3fc0a2ec61a76031edcd6
نام خودت <mamasaremi2@gmail.com>
2026-05-15 03:57:33 +0330

    Add SHIL V15 project page integration components

---
+++ src/components/project/EngineeringInputGrid.jsx
@@ -0,0 +1,11 @@
+﻿import React from "react";
+
+export default function EngineeringInputGrid({
+  children,
+}) {
+  return (
+    <section className="engineering-input-grid-v15">
+      {children}
+    </section>
+  );
+}
