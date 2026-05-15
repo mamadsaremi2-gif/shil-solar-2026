2eabf5bdb76c9376620a35fe1fff7470f970d31b
نام خودت <mamasaremi2@gmail.com>
2026-05-15 03:32:25 +0330

    Add SHIL V15 engineering radar pipeline and technical stats widgets

---
+++ src/components/dashboard/EngineeringStatsWidget.jsx
@@ -0,0 +1,67 @@
+﻿import React from "react";
+
+const stats = [
+
+  {
+    title: "توان سیستم",
+    value: "12.5 kW",
+  },
+
+  {
+    title: "تعداد پنل",
+    value: "24",
+  },
+
+  {
+    title: "ولتاژ DC",
+    value: "480V",
+  },
+
+  {
+    title: "زمان بکاپ",
+    value: "8h",
+  },
+
+];
+
+export default function EngineeringStatsWidget() {
+
+  return (
+
+    <div className="engineering-stats-v15">
+
+      <div className="widget-head-v15">
+
+        <div>
+
+          <span>ENGINEERING DATA</span>
+
+          <h3>مشخصات فنی سیستم</h3>
+
+        </div>
+
+      </div>
+
+      <div className="engineering-stats-grid-v15">
+
+        {stats.map((item) => (
+
+          <div
+            key={item.title}
+            className="engineering-stat-box-v15"
+          >
+
+            <h4>{item.title}</h4>
+
+            <p>{item.value}</p>
+
+          </div>
+
+        ))}
+
+      </div>
+
+    </div>
+
+  );
+}
