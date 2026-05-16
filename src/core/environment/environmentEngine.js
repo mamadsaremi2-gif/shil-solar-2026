fc7329995a9bfbe9e6e1e5ac3ccaf596cadaa0ac
نام خودت <mamasaremi2@gmail.com>
2026-05-16 16:56:16 +0330

    SHIL V5 FINAL STABLE RELEASE

---
+++ src/core/environment/environmentEngine.js
@@ -0,0 +1,13 @@
+﻿export const iranEnvironmentDefaults = {
+  irradiance: 5.2,
+  temperature: 25,
+  altitude: 1200
+};
+
+export function generateEnvironment(city, province) {
+  return {
+    city,
+    province,
+    ...iranEnvironmentDefaults
+  };
+}
---
+++ src/core/scenario/scenarioInjection.js
@@ -0,0 +1,32 @@
+﻿import { scenarioLibrary } from "../../data/scenarios/scenarioLibrary";
+
+export function getScenarioById(id) {
+  return scenarioLibrary.find((item) => item.id === Number(id));
+}
+
+export function injectScenarioIntoProject(scenario, projectType) {
+  return {
+    id: scenario.id,
+    title: scenario.title,
+    type: projectType,
+    level: scenario.level,
+
+    environment: {
+      city: "",
+      province: "",
+      irradiance: null,
+      temperature: null
+    },
+
+    equipment: {
+      inverter: scenario.inverter,
+      batteryType: scenario.batteryType,
+      suggestedBattery: scenario.suggestedBattery,
+      suggestedPanels: scenario.suggestedPanels
+    },
+
+    calculations: {
+      estimatedLoad: scenario.loadEstimate
+    }
+  };
+}
