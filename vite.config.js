aef004c2bda10043ddaaf7446795b4485734dc21
نام خودت <mamasaremi2@gmail.com>
2026-05-21 21:00:41 +0330

    fix blank vercel screen

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,16 @@
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [
-    react()
-  ],
-
-  base: './',
+  plugins: [react()],
 
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
     emptyOutDir: true
+  },
+
+  server: {
+    host: true,
+    port: 5173
   }
 })
