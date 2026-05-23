0516cc50f194aba53ac300ecbafa55322683db51
نام خودت <mamasaremi2@gmail.com>
2026-05-23 09:05:28 +0330

    fix production runtime crash

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
-import { initShilPWA } from "./offline/pwaRuntime.js";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./app/App.jsx";
+
 import "./styles/shil-ui.css";
 import "./styles/app.css";
 import "./styles/shil-ui-final-100.css";
@@ -9,27 +9,9 @@ import "./styles/shil-ux-flow-100.css";
 import "./styles/shil-project-management-100.css";
 
 window.React = React;
-initShilPWA();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-import {
-  startRealtimeSimulation,
-} from "./realtime/streams/startRealtimeSimulation.js";
-
-startRealtimeSimulation();
-import { reportWebVitals } from "./production/performance/webVitals.js";
-import { preloadCriticalAssets } from "./production/performance/preloadAssets.js";
-import { registerGlobalErrorHandlers } from "./production/errors/globalErrors.js";
-
-registerGlobalErrorHandlers();
-preloadCriticalAssets();
-reportWebVitals();
-import { initSentry } from "./enterprise/telemetry/sentry.js";
-import { initTelemetry } from "./enterprise/telemetry/telemetry.js";
-
-initSentry();
-initTelemetry();
