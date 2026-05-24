2ea480271531032bdc9892f2b82e87e300e09251
نام خودت <mamasaremi2@gmail.com>
2026-05-24 13:20:29 +0330

    Finalize SHIL unified PV engine and mobile UI fixes

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-13942bd7'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "/index.html",
-    "revision": "0.4241vpjc1sg"
+    "revision": "0.eh0nvvjdd88"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
